49de363df3c67524a5c5d77d69accb6cc4923bcf
LiliyaRafikova <liliya89fm@mail.ru>
2023-04-22 12:20:39 +0500

    правка 1задачи

--- 2023.04.16/1.py
+++ 2023.04.16/1.py
@@ -2,22 +2,14 @@ num_1 = input('Введите первое число: ')
 num_2 = input('Введите второе число: ')
 num_3 = input('Введите третье число: ')
 summa_positiv = 0
-
-if num_1.isdecimal():
-    summa_positiv += int(num_1)
-elif num_1.replace('.','',1).isdecimal():
+# isdecimal() позволит дополнительно отсеить отрицательные числа, поэтому проверку на > или < 0 не делаем.
+if num_1.replace('.','',1).isdecimal():
     summa_positiv += float(num_1)
-
-if num_2.isdecimal():
-    summa_positiv += int(num_2)
-elif num_2.replace('.','',1).isdecimal():
+if num_2.replace('.','',1).isdecimal():
     summa_positiv += float(num_2)
-
-if num_3.isdecimal():
-    summa_positiv += int(num_3)
-elif num_3.replace('.','',1).isdecimal():
-    summa_positiv += float(num_3)
-    
+if num_3.replace('.','',1).isdecimal():
+    summa_positiv += float(num_3)      
+   
 print(f'{summa_positiv:.1f}')
 
 # Введите первое число: -23
