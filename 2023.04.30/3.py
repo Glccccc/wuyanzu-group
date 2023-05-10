fe526ecf3f4fc5b31622cf1a4576c59b6402706f
LiliyaRafikova <liliya89fm@mail.ru>
2023-05-10 16:27:26 +0500

    убрала find

--- 2023.04.30/3.py
+++ 2023.04.30/3.py
@@ -1,28 +1,29 @@
-# list_num1 = input(' Введите целые положительные числа через пробел: ').split()
-# list_num2 = input(' Введите целые положительные числа через пробел: ').split()
+list_num1 = input(' Введите целые положительные числа через пробел: ').split()
+list_num2 = input(' Введите целые положительные числа через пробел: ').split()
 
-# if list_num2 == '':
-    # print('Да')
-# elif len(list_num1) >= len(list_num2):
-    # try:
-        # if list_num2 == list_num1[list_num1.index((list_num2[0])) : list_num1.index((list_num2[0])) + len(list_num2)]:
-            # print('Да')
-        # else:
-            # print('Нет')
-    # если индекс с элементом не найден
-    # except ValueError:
-        # print('Нет')
-# else:
-    # print('Нет')
-
-# второй способ через метод find, он может искать подстроки, а не только одиночные символы. Если подстрока не найден возвращает -1. 
-list_num1 = input(' Введите целые положительные числа через пробел: ')
-list_num2 = input(' Введите целые положительные числа через пробел: ')
-
-if list_num1.find(list_num2) >= 0:
+if list_num2 == '':
     print('Да')
+elif len(list_num1) >= len(list_num2):
+    try:
+        if list_num2 == list_num1[list_num1.index((list_num2[0])) : list_num1.index((list_num2[0])) + len(list_num2)]:
+            print('Да')
+        else:
+            print('Нет')
+    если индекс с элементом не найден
+    except ValueError:
+        print('Нет')
 else:
     print('Нет')
+
+# второй способ через метод find, он может искать подстроки, а не только одиночные символы. Если подстрока не найден возвращает -1.
+# Этот метод всё таки не используем, т.к. в задание найти подсписок, а не подстроку.  
+# list_num1 = input(' Введите целые положительные числа через пробел: ')
+# list_num2 = input(' Введите целые положительные числа через пробел: ')
+
+# if list_num1.find(list_num2) >= 0:
+    # print('Да')
+# else:
+    # print('Нет')
     
 # 1 2 3 4
 # 1 2
