e94de1c46e4c1229cecd5f8133a9663907a0c16c
LiliyaRafikova <liliya89fm@mail.ru>
2023-05-10 15:34:23 +0500

    Замена на метод find

--- 2023.04.30/3.py
+++ 2023.04.30/3.py
@@ -1,17 +1,26 @@
-list_num1 = input(' Введите целые положительные числа через пробел: ').split()
-list_num2 = input(' Введите целые положительные числа через пробел: ').split()
+# list_num1 = input(' Введите целые положительные числа через пробел: ').split()
+# list_num2 = input(' Введите целые положительные числа через пробел: ').split()
 
-if list_num2 == '':
-    print('Да')
-elif len(list_num1) >= len(list_num2):
-    try:
-        if list_num2 == list_num1[list_num1.index((list_num2[0])) : list_num1.index((list_num2[0])) + len(list_num2)]:
-            print('Да')
-        else:
-            print('Нет')
+# if list_num2 == '':
+    # print('Да')
+# elif len(list_num1) >= len(list_num2):
+    # try:
+        # if list_num2 == list_num1[list_num1.index((list_num2[0])) : list_num1.index((list_num2[0])) + len(list_num2)]:
+            # print('Да')
+        # else:
+            # print('Нет')
     # если индекс с элементом не найден
-    except ValueError:
-        print('Нет')
+    # except ValueError:
+        # print('Нет')
+# else:
+    # print('Нет')
+
+# второй способ через метод find, он может искать подстроки, а не только одиночные символы. Если подстрока не найден возвращает -1. 
+list_num1 = input(' Введите целые положительные числа через пробел: ')
+list_num2 = input(' Введите целые положительные числа через пробел: ')
+
+if list_num1.find(list_num2) >= 0:
+    print('Да')
 else:
     print('Нет')
     
@@ -22,3 +31,8 @@ else:
 # 1 2 3 4
 # 2 4
 # Нет
+
+# проверка если пустая строка
+# Введите целые положительные числа через пробел: 1 2 3 4
+# Введите целые положительные числа через пробел:
+# Да
