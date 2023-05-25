ac4a1959b2db97d6787a071b3b1baa836a256864
LiliyaRafikova <liliya89fm@mail.ru>
2023-05-25 08:17:21 +0500

    убрала условие len(inp_num) >= 2

--- 2023.04.30/6.py
+++ 2023.04.30/6.py
@@ -4,30 +4,36 @@ set_inp_num = set(inp_num)
 
 if inp_num == 'b' or inp_num == '0b' or inp_num == '1b':
     print('Нет')
-elif len(inp_num) >= 2:
-    if set_inp_num <= set_binary_nums and 'b' not in set_inp_num:
-        print('Да')
-    elif (
-            set_inp_num <= set_binary_nums
-        and inp_num[0] != '1'
-        and inp_num.count('b') == 1
-        and (inp_num.index('b') == 1 or inp_num.index('b') == 0)
-    ):   
-        print('Да')
-    else:
-        print('Нет')
+elif set_inp_num <= set_binary_nums and 'b' not in set_inp_num:
+    print('Да')
+elif (
+       set_inp_num <= set_binary_nums
+    and inp_num[0] != '1'
+    and inp_num.count('b') == 1
+    and (inp_num.index('b') == 1 or inp_num.index('b') == 0)
+):   
+    print('Да')
 else:
     print('Нет')
 
+
+
+# Убрала условие len(inp_num) >= 2: почему то всегда думала что 0 в двоичной это 00, а 1 - 01. век живи-век учись))
 # Введите число: 0101
 # Да
-
 # Введите число: b11
 # Да
-
 # Введите число: 0b11001
 # Да
-
 # Введите число: 1b0101
 # Нет
-
+# Введите число: 01
+# Да
+# Введите число: 11
+# Да
+# Введите число: 1
+# Да
+# Введите число: 0
+# Да
+# Введите число: 0b003b
+# Нет
