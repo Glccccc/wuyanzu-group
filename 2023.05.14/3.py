059f6a8dcfb8cccaacb0997d7a8fbf12438f1f24
LiliyaRafikova <liliya89fm@mail.ru>
2023-06-04 14:48:51 +0500

    проверку переключателя добавила до цикла

--- 2023.05.14/3.py
+++ 2023.05.14/3.py
@@ -1,29 +1,32 @@
 def numbers_strip(numbers: list[float], n: int = 1, *, copy: bool = False) -> list:
 
     """Удаляет n минимальных и n максимальных чисел из списка, возвращает исходный объект списка с внесёнными изменениями или изменённую копию"""
-    
-    for _ in range(0, n):
+        
+    if copy:
+        numbers = numbers.copy()
+    for _ in range(n):
         numbers.remove(max(numbers))
         numbers.remove(min(numbers))       
     
-    return numbers.copy() if copy else numbers   
-    # заменила на тернарный оператор
-    # if copy:
-        # return numbers.copy()
-    # else:
-        # return numbers
-       
-     
+    return numbers    
+        
 # >>> nums =[10,20,30,40,50,60,70]
 # >>> nums_test= numbers_strip(nums, 3, copy=True)
 # >>> nums_test
 # [40]
 # >>> nums is nums_test
 # False
+# >>> nums
+# [10, 20, 30, 40, 50, 60, 70]
 # >>>
-# >>> sample = [1,2,3,4,5,6,7]
-# >>> nums_test= numbers_strip(sample)
-# >>> nums_test
-# [2, 3, 4, 5, 6]
-# >>> sample is nums_test
+
+
+# >>> nums =[10,20,30,40,50,60,70]
+# >>> nums_test= numbers_strip(nums)
+# >>> nums
+# [20, 30, 40, 50, 60]
+# >>> nums is nums_test
 # True
+# >>> nums
+# [20, 30, 40, 50, 60]
+# >>>
