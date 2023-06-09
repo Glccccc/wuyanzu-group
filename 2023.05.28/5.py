3107ad277fd452295015bd3c4b6b91e7965a27bc
LiliyaRafikova <liliya89fm@mail.ru>
2023-06-09 09:24:24 +0500

    переписала запись в файл

--- 2023.05.28/5.py
+++ 2023.05.28/5.py
@@ -1,16 +1,13 @@
-from pathlib import Path
 from datetime import datetime as dt
     
 def logger(function: 'callable') -> 'callable':
 
     """Фукнкция декоратор, который в стандартном потоке вывода ведёт журнал вызовов декорируемой функции"""
-    
-    file_path = Path(r'D:\Лилия_мои_доки\Pythom_HW_git\Rafikova\2023.05.28\data\function_calls.log')
-    
+          
     def wrapper(*args, **kwargs) -> 'any':
     
         now = dt.now()
-        now.strftime('%Y.%m.%d %H:%M:%S')
+        today = now.strftime('%Y.%m.%d %H:%M:%S')
         
         arguments =[f'{arg}' for arg in args] + [f'{k}={v}' for k, v in kwargs.items()] 
                 
@@ -23,25 +20,29 @@ def logger(function: 'callable') -> 'callable':
             for k, v in function.__kwdefaults__.items():
                 if k not in kwargs:
                     arguments += [f'{k}={v}']
-                    
-        print_log = f'{now.strftime("%Y.%m.%d %H:%M:%S")} - {function.__name__}({", ".join(arguments)}) -> '
         
-        with open(file_path, mode = 'a', encoding = 'utf-8') as filein:
+        print_log = f'{today} - {function.__name__}({", ".join(arguments)}) -> '
+        
+        with open('data\\function_calls.log', mode = 'a', encoding = 'utf-8') as filein:
             filein.write(print_log)
-        
-        try: 
-            print(result := function(*args, **kwargs))
-            with open(file_path, mode = 'a', encoding = 'utf-8') as filein:
+             
+            try: 
+                result = function(*args, **kwargs)
                 filein.write(f'{result}\n')
-        except Exception as exception:
-            log_exception = f'{type(exception).__name__}: {str(exception)}'
-            print(f'\t{type(exception).__name__}:', str(exception))
-            with open(file_path, mode = 'a', encoding = 'utf-8') as filein:
+                return result
+            except Exception as exception:
+                log_exception = f'{type(exception).__name__}: {str(exception)}'
+                print(f'\t{log_exception}')
                 filein.write(f'{log_exception}\n')             
-     
+          
     return wrapper
     
 # D:\Лилия_мои_доки\Pythom_HW_git\Rafikova\2023.05.28>python -i 5.py
+# >>> def testing_function():
+# ...     pass
+# ...
+# >>> testing_function = logger(testing_function)
+# >>> testing_function()
 # >>> def div_round(num1, num2, *, digits=0):
 # ...     return round(num1 / num2, digits)
 # ...
@@ -50,14 +51,8 @@ def logger(function: 'callable') -> 'callable':
 # 0.6667
 # >>> div_round(2, 0, digits=4)
         # ZeroDivisionError: division by zero
-# >>> def testing_function():
-# ...     pass
-# ...
-# >>> testing_function = logger(testing_function)
-# >>> testing_function()
-# None
 # >>>
 
-# 2023.06.08 15:48:05 - div_round(2, 3, digits=4) -> 0.6667 
-# 2023.06.08 15:48:09 - div_round(2, 0, digits=4) -> ZeroDivisionError: division by zero
-# 2023.06.08 15:49:14 - testing_function() -> None 
+# 2023.06.09 09:17:51 - testing_function() -> None
+# 2023.06.09 09:18:26 - div_round(2, 3, digits=4) -> 0.6667
+# 2023.06.09 09:18:33 - div_round(2, 0, digits=4) -> ZeroDivisionError: division by zero
