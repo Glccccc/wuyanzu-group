636a000ba8192abe2ad52ee03a0fd1bfac4339cf
LiliyaRafikova <liliya89fm@mail.ru>
2023-06-16 09:54:57 +0500

    Переписала ф-ию product и исправление в 3 задаче

--- 2023.06.04/1.3.py
+++ 2023.06.04/1.3.py
@@ -5,29 +5,28 @@ def tree_generator() -> list:
     """Генерирует дерево с произвольным количеством веток и листьев. Возвразщает список с произвольной глубиной вложенности"""
     
     tree = []
-   
+    
     if randrange(2):
-        tree += ['leaf'] * randrange(1,4) 
+        tree += ['leaf'] * randrange(1,5)
     else:        
-        for _ in range(randrange(0,5)):
+        for _ in range(randrange(1,4)):
             tree.append(tree_generator())
     
     return tree
 
 
 
+# [[[['leaf', 'leaf'], [['leaf'], ['leaf', 'leaf']]], ['leaf', 'leaf', 'leaf']]]
 # >>> tree_generator()
-# ['leaf', 'leaf', 'leaf']
+# ['leaf', 'leaf', 'leaf', 'leaf']
 # >>> tree_generator()
-# ['leaf']
+# ['leaf', 'leaf']
 # >>> tree_generator()
-# []
+# [[['leaf', 'leaf', 'leaf', 'leaf']]]
 # >>> tree_generator()
-# [['leaf', 'leaf']]
+# [['leaf', 'leaf'], ['leaf', 'leaf', 'leaf', 'leaf']]
 # >>> tree_generator()
-# [[['leaf'], [['leaf']]], ['leaf'], [['leaf', 'leaf', 'leaf'], ['leaf', 'leaf', 'leaf'], ['leaf', 'leaf'], ['leaf']]]
+# [['leaf', 'leaf', 'leaf', 'leaf'], ['leaf', 'leaf', 'leaf'], ['leaf']]
 # >>> tree_generator()
-# [[['leaf'], ['leaf', 'leaf'], [[[[[[['leaf']], ['leaf'], [['leaf', 'leaf', 'leaf'], [['leaf'], ['leaf'], ['leaf'], ['leaf']]]]], [[], ['leaf'], ['leaf'], [[], [[['leaf'], [[['leaf', 'leaf']], []], [[['leaf', 'leaf'], ['leaf'], ['leaf']], ['leaf', 'leaf'], ['leaf'], ['leaf']]], ['leaf'], ['leaf', 'leaf', 'leaf']], ['leaf'], [['leaf', 'leaf']]]]], [['leaf', 'leaf'], ['leaf', 'leaf'], [[[[['leaf', 'leaf', 'leaf'], ['leaf'], ['leaf', 'leaf'], ['leaf', 'leaf', 'leaf']], ['leaf', 'leaf', 'leaf']], ['leaf', 'leaf', 'leaf']]], ['leaf', 'leaf']], [['leaf', 'leaf', 'leaf'], [['leaf'], [['leaf'], ['leaf', 'leaf', 'leaf']], ['leaf', 'leaf']], []], [[['leaf'], [['leaf']], ['leaf', 'leaf', 'leaf']], [[], [['leaf', 'leaf', 'leaf'], [[[['leaf', 'leaf', 'leaf'], [[[['leaf'], [], ['leaf', 'leaf'], ['leaf']], ['leaf'], [], [['leaf', 'leaf'], ['leaf'], [['leaf', 'leaf', 'leaf'], ['leaf', 'leaf'], [], ['leaf', 'leaf', 'leaf']], [['leaf'], ['leaf', 'leaf', 'leaf']]]], ['leaf']], [['leaf', 'leaf', 'leaf'], [], ['leaf', 'leaf'], []], ['leaf', 'leaf', 'leaf']], [[[[[['leaf', 'leaf']], [[['leaf', 'leaf'], ['leaf'], ['leaf', 'leaf']], [[[['leaf', 'leaf', 'leaf']], ['leaf']]], [[['leaf'], ['leaf'], [['leaf', 'leaf']]]]], ['leaf', 'leaf']]]]], [['leaf', 'leaf'], ['leaf', 'leaf'], [[['leaf'], [['leaf', 'leaf'], [[[['leaf', 'leaf'], [['leaf'], [], [[], ['leaf'], ['leaf', 'leaf']]], [['leaf', 'leaf', 'leaf'], ['leaf']]], ['leaf', 'leaf', 'leaf'], []], ['leaf', 'leaf', 'leaf']], ['leaf', 'leaf']]], [['leaf', 'leaf', 'leaf']], []]]]]], ['leaf', 'leaf', 'leaf']], [['leaf', 'leaf', 'leaf'], [['leaf']]], ['leaf', 'leaf', 'leaf']]], [['leaf'], [], [['leaf'], ['leaf', 'leaf', 'leaf'], ['leaf'], [['leaf', 'leaf', 'leaf'], ['leaf', 'leaf', 'leaf']]]], ['leaf', 'leaf'], []]], ['leaf']]
-# >>> tree_generator()
-# [['leaf'], ['leaf', 'leaf'], ['leaf', 'leaf']]
+# [[['leaf', 'leaf', 'leaf'], ['leaf', 'leaf'], [[['leaf', 'leaf', 'leaf'], [['leaf', 'leaf'], ['leaf'], [['leaf']]]], [[['leaf', 'leaf']], ['leaf'], ['leaf', 'leaf']]]], ['leaf', 'leaf']]
 
