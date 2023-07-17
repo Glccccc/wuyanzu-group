32a07f3db89626c0e3b047dd459a58ad7ecc8ee7
LiliyaRafikova <liliya89fm@mail.ru>
2023-07-17 16:14:14 +0500

    правка в class CountableNouns: возврат метода pick

--- 2023.07.02/4.py
+++ 2023.07.02/4.py
@@ -16,91 +16,85 @@ class CountableNouns:
 
   
     @classmethod
-    def pick(self, number: int, word: str) -> str:
+    def pick(cls, number: int, word: str) -> str:
        
         """Принимает в качестве аргументов число и существительное для согласования в единственном числе, возвращает согласованное с переданным числом существительное"""
         
-        if word in self.words:          
+        if word not in cls.words:
+            cls.words[word] = tuple()
+            return word                 
+        
+        elif word in cls.words and cls.words[word]:        
             list_1= [2, 3, 4]
             list_2 = [0, 5, 6, 7, 8, 9]
 
             if 11 <= number % 100 <=14 or number % 10 in list_2:
-                return self.words[word][1]
+                return cls.words[word][1]
             elif number % 10 in list_1:
-                return self.words[word][0]
+                return cls.words[word][0]
             else:
                 return word
         else:
-            self.save_words(word)
+            print(f'существительное "{word}" отсутствует в базе')
+            cls.save_words(word)
        
     @classmethod
-    def save_words(self, word1: str = None) -> None:
+    def save_words(cls, word1: str = None) -> None:
     
         """Запрашивает в stdin у пользователя два или три слова согласующихся с числительными, добавляет полученные значения в поле класса words и дописывает их в файл с базой существительных"""
        
-        if word1 is not None:
-            print(f'существительное \"{word1}" отсутствует в базе')        
         if word1 is None:
             word1 = input('введите слово, согласующееся с числительным "один": ')  
             
         word2: str = input(' введите слово, согласующееся с числительным "два": ')
         word3: str = input(' введите слово, согласующееся с числительным "пять": ')
         
-        self.words[word1] = tuple([word2, word3])  
+        cls.words[word1] = tuple([word2, word3])  
 
-        with open(self.db_path, 'w', encoding='utf-8') as w_file:
+        with open(cls.db_path, 'w', encoding='utf-8') as w_file:
             row = csv.writer(w_file, delimiter = ",", lineterminator='\n')
-            row.writerows([k, *v] for k, v in self.words.items())
+            row.writerows([k, *v] for k, v in cls.words.items())
           
-
 # >>> CountableNouns.words
 # {'год': ('года', 'лет'), 'месяц': ('месяца', 'месяцев'), 'день': ('дня', 'дней')}
+# >>> CountableNouns.pick(12,'год')
+# 'лет'
 # >>> CountableNouns.pick(1,'год')
 # 'год'
-# >>> CountableNouns.pick(8,'год')
-# 'лет'
 # >>> CountableNouns.pick(22,'год')
 # 'года'
-# >>> CountableNouns.pick(365,'день')
-# 'дней'
-# >>> CountableNouns.pick(2,'день')
-# 'дня'
-# >>> CountableNouns.pick(1,'день')
-# 'день'
 # >>> CountableNouns.pick(3,'месяц')
 # 'месяца'
-# >>> CountableNouns.pick(14,'месяц')
+# >>> CountableNouns.pick(16,'месяц')
 # 'месяцев'
-# >>> CountableNouns.pick(11,'месяц')
-# 'месяцев'
-# >>> CountableNouns.pick(1,'месяц')
-# 'месяц'
-# >>> CountableNouns.pick(10,'машина')
-# существительное "машина" отсутствует в базе
- # введите слово, согласующееся с числительным "два": машины
- # введите слово, согласующееся с числительным "пять": машин
-# >>> CountableNouns.pick(10,'машина')
-# 'машин'
-# >>> CountableNouns.pick(22,'машина')
-# 'машины'
-# >>> CountableNouns.pick(33,'капля')
-# существительное "капля" отсутствует в базе
- # введите слово, согласующееся с числительным "два": капли
- # введите слово, согласующееся с числительным "пять": капель
-# >>> CountableNouns.words
-# {'год': ('года', 'лет'), 'месяц': ('месяца', 'месяцев'), 'день': ('дня', 'дней'), 'машина': ('машины', 'машин'), 'капля': ('капли', 'капель')}
+# >>> CountableNouns.pick(16,'день')
+# 'дней'
+# >>> CountableNouns.pick(1,'день')
+# 'день'
+# >>> CountableNouns.pick(11,'банан')
+# 'банан'
+# >>> CountableNouns.pick(11,'банан')
+# существительное "банан" отсутствует в базе
+ # введите слово, согласующееся с числительным "два": банана
+ # введите слово, согласующееся с числительным "пять": бананов
+# >>> CountableNouns.pick(11,'банан')
+# 'бананов'
 # >>> CountableNouns.save_words()
-# введите слово, согласующееся с числительным "один": попугай
- # введите слово, согласующееся с числительным "два": попугая
- # введите слово, согласующееся с числительным "пять": попугаев
-# >>> CountableNouns.words
-# {'год': ('года', 'лет'), 'месяц': ('месяца', 'месяцев'), 'день': ('дня', 'дней'), 'машина': ('машины', 'машин'), 'капля': ('капли', 'капель'), 'попугай': ('попугая', 'попугаев')}
+# введите слово, согласующееся с числительным "один": стол
+ # введите слово, согласующееся с числительным "два": стола
+ # введите слово, согласующееся с числительным "пять": столов
+# >>> CountableNouns.pick(11,'стол')
+# 'столов'
+# >>> CountableNouns.pick(2,'стол')
+# 'стола'
 # >>> print(CountableNouns.db_path.read_text(encoding='utf-8'))
 # год,года,лет
 # месяц,месяца,месяцев
 # день,дня,дней
-# машина,машины,машин
-# капля,капли,капель
-# попугай,попугая,попугаев
+# банан,банана,бананов
+# стол,стола,столов
 
+# >>> CountableNouns.words
+# {'год': ('года', 'лет'), 'месяц': ('месяца', 'месяцев'), 'день': ('дня', 'дней'), 'банан': ('банана', 'бананов'), 'стол': ('стола', 'столов')}
 # >>>
+
